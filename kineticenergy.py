1714dc223d462b202746c0d0ed98f0ce05bdb574
Ankur <64908229+Ankur-72@users.noreply.github.com>
2020-09-27 17:01:51 +0530

    Add files via upload

---
+++ kineticenergy.py
@@ -0,0 +1,6 @@
+print("Please provide the value of mass (m)")
+mass = int(input())
+print("Please provide the value of velocity (v)")
+velocity = int(input())
+kineticenergy = 1/2*mass*velocity*velocity
+print(kineticenergy)
